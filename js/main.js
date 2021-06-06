26b8240b2554b0e8efb12d8b7ac398bd3133b8f3
VenMartin <55118001+venmartin@users.noreply.github.com>
2021-06-06 15:49:35 +1000

    Updated Weather App

--- js/main.js
+++ js/main.js
@@ -180,6 +180,7 @@ One location = http://api.openweathermap.org/data/2.5/weather?q=Sydney&units=met
 
 5 day forecast = https://api.openweathermap.org/data/2.5/onecall?lat=-33.98&lon=-151.12&exclude=hourly,daily&appid=7b069d76e3865c86d3513410c18a4226
 */
+const weatherModalBg = document.querySelector('.modal__bg__img');
 
 let weatherOriginal = {
   apiKey: "7b069d76e3865c86d3513410c18a4226",
@@ -221,6 +222,10 @@ let weatherOriginal = {
         weatherIcons.forEach(function(item){
           item.src = `icons/${icon}.svg`
         });
+
+      
+      weatherModalBg.style.backgroundImage = `url('img/${icon}.jpg')`;
+      
       
       document.querySelector('.city').innerText = `${name}, ${country}`;
       document.querySelector('.description').innerText = description;
@@ -228,8 +233,29 @@ let weatherOriginal = {
         weatherTemp.forEach(function(item){
           item.innerText = `${Math.round(temp)}°C`;
         });
-      document.querySelector('.humidity').innerText = `Humidity: ${humidity}%`;
-      document.querySelector('.wind').innerText = `Wind Speed: ${speed}km/h`;
+      document.querySelector('.humidity').innerHTML = `
+      <svg stroke="currentColor" fill="currentColor" stroke-width="0" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 30 30" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M7.56,17.19c0-0.88,0.24-1.89,0.72-3.03s1.1-2.25,1.86-3.31c1.56-2.06,2.92-3.62,4.06-4.67l0.75-0.72
+	c0.25,0.26,0.53,0.5,0.83,0.72c0.41,0.42,1.04,1.11,1.88,2.09s1.57,1.85,2.17,2.65c0.71,1.01,1.32,2.1,1.81,3.25
+	s0.74,2.16,0.74,3.03c0,1-0.19,1.95-0.58,2.86c-0.39,0.91-0.91,1.7-1.57,2.36c-0.66,0.66-1.45,1.19-2.37,1.58
+	c-0.92,0.39-1.89,0.59-2.91,0.59c-1,0-1.95-0.19-2.86-0.57c-0.91-0.38-1.7-0.89-2.36-1.55c-0.66-0.65-1.19-1.44-1.58-2.35
+	S7.56,18.23,7.56,17.19z M9.82,14.26c0,0.83,0.17,1.49,0.52,1.99c0.35,0.49,0.88,0.74,1.59,0.74c0.72,0,1.25-0.25,1.61-0.74
+	c0.35-0.49,0.53-1.15,0.54-1.99c-0.01-0.84-0.19-1.5-0.54-2c-0.35-0.49-0.89-0.74-1.61-0.74c-0.71,0-1.24,0.25-1.59,0.74
+	C9.99,12.76,9.82,13.42,9.82,14.26z M11.39,14.26c0-0.15,0-0.27,0-0.35s0.01-0.19,0.02-0.33c0.01-0.14,0.02-0.25,0.05-0.32
+	s0.05-0.16,0.09-0.24c0.04-0.08,0.09-0.15,0.15-0.18c0.07-0.04,0.14-0.06,0.23-0.06c0.14,0,0.25,0.04,0.33,0.12s0.14,0.21,0.17,0.38
+	c0.03,0.18,0.05,0.32,0.06,0.45s0.01,0.3,0.01,0.52c0,0.23,0,0.4-0.01,0.52c-0.01,0.12-0.03,0.27-0.06,0.45
+	c-0.03,0.17-0.09,0.3-0.17,0.38s-0.19,0.12-0.33,0.12c-0.09,0-0.16-0.02-0.23-0.06c-0.07-0.04-0.12-0.1-0.15-0.18
+	c-0.04-0.08-0.07-0.17-0.09-0.24c-0.02-0.08-0.04-0.19-0.05-0.32c-0.01-0.14-0.02-0.25-0.02-0.32S11.39,14.41,11.39,14.26z
+	 M11.98,22.01h1.32l4.99-10.74h-1.35L11.98,22.01z M16.28,19.02c0.01,0.84,0.2,1.5,0.55,2c0.35,0.49,0.89,0.74,1.6,0.74
+	c0.72,0,1.25-0.25,1.6-0.74c0.35-0.49,0.52-1.16,0.53-2c-0.01-0.84-0.18-1.5-0.53-1.99c-0.35-0.49-0.88-0.74-1.6-0.74
+	c-0.71,0-1.25,0.25-1.6,0.74C16.47,17.52,16.29,18.18,16.28,19.02z M17.85,19.02c0-0.23,0-0.4,0.01-0.52
+	c0.01-0.12,0.03-0.27,0.06-0.45s0.09-0.3,0.17-0.38s0.19-0.12,0.33-0.12c0.09,0,0.17,0.02,0.24,0.06c0.07,0.04,0.12,0.1,0.16,0.19
+	c0.04,0.09,0.07,0.17,0.1,0.24s0.04,0.18,0.05,0.32l0.01,0.32l0,0.34c0,0.16,0,0.28,0,0.35l-0.01,0.32l-0.05,0.32l-0.1,0.24
+	l-0.16,0.19l-0.24,0.06c-0.14,0-0.25-0.04-0.33-0.12s-0.14-0.21-0.17-0.38c-0.03-0.18-0.05-0.33-0.06-0.45S17.85,19.25,17.85,19.02z
+	"></path></svg>
+       | ${humidity}%`;
+      document.querySelector('.wind').innerHTML = `
+      <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"></path><path d="M10.5 17H4v-2h6.5a3.5 3.5 0 1 1-3.278 4.73l1.873-.703A1.5 1.5 0 1 0 10.5 17zM5 11h13.5a3.5 3.5 0 1 1-3.278 4.73l1.873-.703A1.5 1.5 0 1 0 18.5 13H5a3 3 0 0 1 0-6h8.5a1.5 1.5 0 1 0-1.405-2.027l-1.873-.702A3.501 3.501 0 0 1 17 5.5 3.5 3.5 0 0 1 13.5 9H5a1 1 0 1 0 0 2z"></path></g></svg>
+      | ${speed}km/h`;
       // document.querySelector('.weather').classList.remove('loading');
       // document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?city%20of%20" + name + "')";
       // document.body.style.backgroundImage = `url("https://source.unsplash.com/1600x900/?${name}")`;
@@ -244,10 +270,6 @@ let weatherOriginal = {
 document
   .querySelector('.searchbtn')
   .addEventListener('click', function () {
-    let inputShape = document.querySelector('.card');
-    let cardTimeShape = document.querySelector('.card-time');
-    cardTimeShape.classList.remove('.card-time');
-    inputShape.classList.remove('.card');
     let emptyBox = document.querySelector('input');
       emptyBox.innerHTML = '';
       weather.search();
@@ -265,7 +287,6 @@ document
 // Weather Forecast 7 days App
 const currentTemp = document.getElementById('current__forecast__small');
 const forecastItem = document.getElementById('weather__forecast');
-
 
 const weather7Day = {
   apiKey: "7b069d76e3865c86d3513410c18a4226",
@@ -296,25 +317,27 @@ const weather7Day = {
       data.daily.forEach((day, index) => {
         if (index == 0) {
           
-          currentTemp.innerHTML = `          
-          <div id='current__forecast__small' class="weather__forecast__item">
-           <img src='icons/${day.weather[0].icon}.svg' alt="" class='icon icon__small'>
-           <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
-           <span class='divider'></span>
-           <div class="temp small__temp">${Math.ceil(day.temp.day)}</div>
-               <div class="min__max__wrapper">
-                 <div class="small__min">${Math.round(day.temp.min)}</div>
-                 <div class="small__max">${Math.round(day.temp.max)}</div>
-              </div>
-            <div class='daily__desc'>
-              <div class='daily__humidity'>${day.humidity}%</div>
-              <div class='daily__windspeed'>${day.wind_speed}km/H</div>
-            </div>
-          `
+          // currentTemp.innerHTML = `          
+          // <div id='current__forecast__small' class="weather__forecast__item">
+          //  <img src='icons/${day.weather[0].icon}.svg' alt="" class='icon icon__small'>
+          //  <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
+          //  <span class='divider'></span>
+          //  <div class="temp small__temp">${Math.round(day.temp.day)}</div>
+          //      <div class="min__max__wrapper">
+          //        <div class="small__min">${Math.round(day.temp.min)}</div>
+          //        <div class="small__max">${Math.round(day.temp.max)}</div>
+          //     </div>
+          //   <div class='daily__desc'>
+          //     <div class='daily__humidity'>${day.humidity}%</div>
+          //     <div class='daily__windspeed'>${day.wind_speed}km/H</div>
+          //   </div>
+          // `
+
+
         } else {
                    
           everyOtherDay += `
-          <div id='current__forecast__small' class="weather__forecast__item">
+          <div id='weather__forecast'>
           <div class="weather__forecast__item">
             <img src='icons/${day.weather[0].icon}.svg' alt="" class='d3 icon__small'>
               <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
@@ -325,8 +348,29 @@ const weather7Day = {
               <div class="small__max">${Math.round(day.temp.max)}</div>
             </div>
             <div class='daily__desc'>
-              <div class='daily__humidity'>${day.humidity}%</div>
-              <div class='daily__windspeed'>${day.wind_speed}km/H</div>
+              <div class='daily__humidity'>
+              <svg stroke="currentColor" fill="currentColor" stroke-width="0" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 30 30" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M7.56,17.19c0-0.88,0.24-1.89,0.72-3.03s1.1-2.25,1.86-3.31c1.56-2.06,2.92-3.62,4.06-4.67l0.75-0.72
+	c0.25,0.26,0.53,0.5,0.83,0.72c0.41,0.42,1.04,1.11,1.88,2.09s1.57,1.85,2.17,2.65c0.71,1.01,1.32,2.1,1.81,3.25
+	s0.74,2.16,0.74,3.03c0,1-0.19,1.95-0.58,2.86c-0.39,0.91-0.91,1.7-1.57,2.36c-0.66,0.66-1.45,1.19-2.37,1.58
+	c-0.92,0.39-1.89,0.59-2.91,0.59c-1,0-1.95-0.19-2.86-0.57c-0.91-0.38-1.7-0.89-2.36-1.55c-0.66-0.65-1.19-1.44-1.58-2.35
+	S7.56,18.23,7.56,17.19z M9.82,14.26c0,0.83,0.17,1.49,0.52,1.99c0.35,0.49,0.88,0.74,1.59,0.74c0.72,0,1.25-0.25,1.61-0.74
+	c0.35-0.49,0.53-1.15,0.54-1.99c-0.01-0.84-0.19-1.5-0.54-2c-0.35-0.49-0.89-0.74-1.61-0.74c-0.71,0-1.24,0.25-1.59,0.74
+	C9.99,12.76,9.82,13.42,9.82,14.26z M11.39,14.26c0-0.15,0-0.27,0-0.35s0.01-0.19,0.02-0.33c0.01-0.14,0.02-0.25,0.05-0.32
+	s0.05-0.16,0.09-0.24c0.04-0.08,0.09-0.15,0.15-0.18c0.07-0.04,0.14-0.06,0.23-0.06c0.14,0,0.25,0.04,0.33,0.12s0.14,0.21,0.17,0.38
+	c0.03,0.18,0.05,0.32,0.06,0.45s0.01,0.3,0.01,0.52c0,0.23,0,0.4-0.01,0.52c-0.01,0.12-0.03,0.27-0.06,0.45
+	c-0.03,0.17-0.09,0.3-0.17,0.38s-0.19,0.12-0.33,0.12c-0.09,0-0.16-0.02-0.23-0.06c-0.07-0.04-0.12-0.1-0.15-0.18
+	c-0.04-0.08-0.07-0.17-0.09-0.24c-0.02-0.08-0.04-0.19-0.05-0.32c-0.01-0.14-0.02-0.25-0.02-0.32S11.39,14.41,11.39,14.26z
+	 M11.98,22.01h1.32l4.99-10.74h-1.35L11.98,22.01z M16.28,19.02c0.01,0.84,0.2,1.5,0.55,2c0.35,0.49,0.89,0.74,1.6,0.74
+	c0.72,0,1.25-0.25,1.6-0.74c0.35-0.49,0.52-1.16,0.53-2c-0.01-0.84-0.18-1.5-0.53-1.99c-0.35-0.49-0.88-0.74-1.6-0.74
+	c-0.71,0-1.25,0.25-1.6,0.74C16.47,17.52,16.29,18.18,16.28,19.02z M17.85,19.02c0-0.23,0-0.4,0.01-0.52
+	c0.01-0.12,0.03-0.27,0.06-0.45s0.09-0.3,0.17-0.38s0.19-0.12,0.33-0.12c0.09,0,0.17,0.02,0.24,0.06c0.07,0.04,0.12,0.1,0.16,0.19
+	c0.04,0.09,0.07,0.17,0.1,0.24s0.04,0.18,0.05,0.32l0.01,0.32l0,0.34c0,0.16,0,0.28,0,0.35l-0.01,0.32l-0.05,0.32l-0.1,0.24
+	l-0.16,0.19l-0.24,0.06c-0.14,0-0.25-0.04-0.33-0.12s-0.14-0.21-0.17-0.38c-0.03-0.18-0.05-0.33-0.06-0.45S17.85,19.25,17.85,19.02z
+	"></path></svg>
+              ${day.humidity}%</div>
+              <div class='daily__windspeed'>
+              <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M9.59 4.59A2 2 0 1 1 11 8H2m10.59 11.41A2 2 0 1 0 14 16H2m15.73-8.27A2.5 2.5 0 1 1 19.5 12H2"></path></svg>
+              ${day.wind_speed}km/h</div>
             </div>
         </div>
         </div>`
@@ -337,8 +381,12 @@ const weather7Day = {
       
 
       
-      document.querySelector('.sunrise').innerText = `Sunrise: ${window.moment(sunrise*1000).format('HH:mm a')}`;
-      document.querySelector('.sunset').innerText = `Sunrise: ${window.moment(sunset*1000).format('HH:mm a')}`;
+      document.querySelector('.sunrise').innerHTML = `
+      <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M256 32l-64 80h32v64h64v-64h32l-64-80zm-9 187v80h18v-80h-18zm-63.992 53.602l-16.631 6.886 15.309 36.955 16.628-6.886-15.306-36.955zm145.984 0l-15.306 36.955 16.628 6.886 15.309-36.955-16.63-6.886zM77.795 284.068l-12.727 12.727 56.569 56.568 12.726-12.726-56.568-56.569zm356.41 0l-56.568 56.569 12.726 12.726 56.569-56.568-12.727-12.727zM256 337.994a118.919 118.919 0 0 0-59.5 15.95c-34.215 19.754-56.177 55.048-59.129 94.056H374.63c-2.952-39.008-24.914-74.302-59.129-94.057a118.919 118.919 0 0 0-59.5-15.949zM66.488 387.377l-6.886 16.63 36.955 15.307 6.886-16.628-36.955-15.309zm379.024 0l-36.955 15.309 6.886 16.628 36.955-15.306-6.886-16.631zM24 466v18h464v-18H24z"></path></svg>
+       | ${window.moment(sunrise*1000).format('HH:mm a')}`;
+      document.querySelector('.sunset').innerHTML = `
+      <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M247 27v80h18V27h-18zm-63.992 53.602l-16.631 6.886 15.309 36.955 16.628-6.886-15.306-36.955zm145.984 0l-15.306 36.955 16.628 6.886 15.309-36.955-16.63-6.886zM77.795 92.068l-12.727 12.727 56.569 56.568 12.726-12.726-56.568-56.569zm356.41 0l-56.568 56.569 12.726 12.726 56.569-56.568-12.727-12.727zM256 145.994a118.919 118.919 0 0 0-59.5 15.95c-34.215 19.754-56.177 55.048-59.129 94.056H374.63c-2.952-39.008-24.914-74.302-59.129-94.057a118.919 118.919 0 0 0-59.5-15.949zM66.488 195.377l-6.886 16.63 36.955 15.307 6.886-16.628-36.955-15.31zm379.024 0l-36.955 15.309 6.886 16.628 36.955-15.306-6.886-16.631zM24 274v18h464v-18H24zm200 62v64h-32l64 80 64-80h-32v-64h-64z"></path></svg>
+      | ${window.moment(sunset*1000).format('HH:mm a')}`;
 
       // document.querySelector('.city').innerText = `Weather in ${timezone}`;
       // document.querySelector('.icon').src = `https://openweathermap.org/img/w/${icon}.png`
